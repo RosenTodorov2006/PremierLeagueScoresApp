c948efb66b4b7bb08cf2bdb1571e412930a5164a
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2024-06-10 19:09:18 +0300

    Add Class

---
+++ src/main/java/bg/soft_uni/premierlegueapp/Models/Entities/Town.java
@@ -0,0 +1,29 @@
+package bg.soft_uni.premierlegueapp.Models.Entities;
+
+import jakarta.persistence.*;
+
+@Table
+@Entity(name = "towns")
+public class Town extends BaseEntity{
+    @Column(nullable = false)
+    private String name;
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id", name = "country_id")
+    private Country country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+}
