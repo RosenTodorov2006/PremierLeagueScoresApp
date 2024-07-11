007a760237de2d0c28289dcb20fa81e78f9c91e5
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2024-07-11 13:19:17 +0300

    Add internationalization for home page

---
+++ src/main/java/bg/soft_uni/premierlegueapp/Configuration/I18NConfiguration.java
@@ -0,0 +1,30 @@
+package bg.soft_uni.premierlegueapp.Configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class I18NConfiguration {
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new CookieLocaleResolver("lang");
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
