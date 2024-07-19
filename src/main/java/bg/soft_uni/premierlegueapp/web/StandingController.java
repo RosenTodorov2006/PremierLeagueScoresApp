ba7018ce2f4ee565a65a0ca70ec604033cc34d7c
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2024-07-19 18:59:10 +0300

    Add monitoring with aop

--- src/main/java/bg/soft_uni/premierlegueapp/web/StandingController.java
+++ src/main/java/bg/soft_uni/premierlegueapp/web/StandingController.java
@@ -3,6 +3,7 @@ package bg.soft_uni.premierlegueapp.web;
 import bg.soft_uni.premierlegueapp.models.dtos.MatchDto;
 import bg.soft_uni.premierlegueapp.models.dtos.PositionSeedDto;
 import bg.soft_uni.premierlegueapp.services.PositionService;
+import bg.soft_uni.premierlegueapp.web.aop.WarnIfExecutionExceeds;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +19,17 @@ public class StandingController {
         this.positionService = positionService;
     }
 
-
+    @WarnIfExecutionExceeds(
+            threshold = 2000
+    )
     @GetMapping("/standings")
     public List<PositionSeedDto> getStandings() {
         List<PositionSeedDto> standing = positionService.getStanding();
         return standing;
     }
-
+    @WarnIfExecutionExceeds(
+            threshold = 2000
+    )
     @GetMapping("/last-matches")
     public List<MatchDto> getLastMatches() {
         return positionService.getLastMatches();
---
+++ src/main/java/bg/soft_uni/premierlegueapp/web/aop/MonitoringAspect.java
@@ -0,0 +1,43 @@
+package bg.soft_uni.premierlegueapp.web.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+public class MonitoringAspect {
+    private final static Logger LOGGER = LoggerFactory.getLogger(MonitoringAspect.class);
+    @Pointcut("@annotation(WarnIfExecutionExceeds)")
+    void onWarnExecutionTimeExceeds(){}
+    @Around("onWarnExecutionTimeExceeds()")
+    Object monitorExecutionTime(ProceedingJoinPoint pjp) throws Throwable {
+        WarnIfExecutionExceeds annotation = getAnnotation(pjp);
+        long threshold = annotation.threshold();
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object proceed = pjp.proceed();
+
+        stopWatch.stop();
+        long methodExecutionTime = stopWatch.lastTaskInfo().getTimeMillis();
+        if(methodExecutionTime>threshold){
+            LOGGER.warn("The method {} executed in {} millis which is more than the acceptable threshold of {} millis."
+                    , pjp.getSignature(), methodExecutionTime, threshold);
+        }
+        return proceed;
+    }
+    private static WarnIfExecutionExceeds getAnnotation(ProceedingJoinPoint pjp){
+        Method method = ((MethodSignature) pjp.getSignature()).getMethod();
+        return method.getAnnotation(WarnIfExecutionExceeds.class);
+    }
+}
