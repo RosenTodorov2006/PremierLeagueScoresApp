081139ef699818f9003491eafde673ffc8057f13
Rosen Todorov <122826159+RosenTodorov2006@users.noreply.github.com>
2024-10-22 00:38:00 +0300

    change api key field

--- src/main/resources/static/js/indexJs.js
+++ src/main/resources/static/js/indexJs.js
@@ -1,4 +1,4 @@
-//ToDo
+
 this.document.getElementById('teamsPages').addEventListener('click', function(e){
     if(document.getElementById('teams-logos').style.display==='none'){
         document.getElementById('teams-logos').classList.toggle("show-ul");
